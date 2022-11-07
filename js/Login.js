b92df82bb5b4c67b12235435b9881f85166bd486
Samuca <35107922+Samuuuca@users.noreply.github.com>
2022-11-06 23:02:00 -0300

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -6,19 +6,19 @@ function Login(){
     localStorage.setItem('Usuario', User.value);
 
     if(User.value == 'Alice' && decodificador(Password.value) == 'Alice' ){ //Alice
-        window.location.replace("index.html");
+        window.location.replace("pages/Ficha.html");
     }
     else if(User.value == 'Mathues' && decodificador(Password.value) == 'Matheus' ){ // Mathues
-        window.location.replace("index.html");
+        window.location.replace("pages/Ficha.html");
     }
     else if(User.value == 'Millena' && decodificador(Password.value) == 'Millena' ){ // Millena
-        window.location.replace("index.html");
+        window.location.replace("pages/Ficha.html");
     }
     else if(User.value == 'Manuela' && decodificador(Password.value) == 'Manuela' ){ // Manu
-        window.location.replace("index.html");
+        window.location.replace("pages/Ficha.html");
     }
     else if(User.value == 'Nathalia' && decodificador(Password.value) == 'Nathalia' ){ // Nathalia
-        window.location.replace("index.html");
+        window.location.replace("pages/Ficha.html");
     }
     else{
         alert('Usuario ou senhas Incorretos')
